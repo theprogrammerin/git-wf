bad4a7546956459b3cae1a5b45344a3bc46d2150
Ashutosh Agrawal <ashutosh_agrawal@live.in>
2015-08-22 02:43:19 +0530

    adding status page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :git_webhooks, only: [:create]
 
+  get '/status' => "application#status"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
